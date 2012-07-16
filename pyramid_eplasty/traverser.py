899f325d3e912631efec3abc60381925aadd71b5
Szymon Pyżalski <szymon.pyzalski@stxnext.pl>
2012-07-16 06:50:29 +0200

    Fixed traverser

--- pyramid_eplasty/traverser.py
+++ pyramid_eplasty/traverser.py
@@ -6,13 +6,10 @@ class Traverser(metaclass=abc.ABCMeta):
     """Traversers wrap around object classes and allow dictionary-like
     lookup"""
 
-    @abc.abstractproperty
-    def class_(self):
-        pass
-
-    @abc.abstractproperty
-    def field(self):
-        pass
+    def __init__(self, class_, field, session=None):
+        self.class_ = class_
+        self.field = field
+        self.session = session
     
     def mount(self, parent, name):
         self.__parent__ = weakref.proxy(parent)
@@ -21,7 +18,9 @@ class Traverser(metaclass=abc.ABCMeta):
 
     def __getitem__(self, key):
         try:
-            result = self.class_.get(self.field == key, session=self.session)
+            result = self.class_.get(
+                getattr(self.class_, self.field) == key, session=self.session
+            )
         except NotFound:
             raise KeyError(key)
         result.__parent__ = self
